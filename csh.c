1eea017aee2a97a8d0707c82695cb5294465e79e
chetannnnnnnnnnnn <chetankumar5102009@gmail.com>
2025-03-28 12:33:43 +0530

    compeleted split token function

--- csh.c
+++ csh.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 #define lsh_bufsize 1024
- 
+#define CSH_TOK_BUFSIZE 64
+#define CSH_TOK_DELIM " \t\r\n\a"
 int int main(int argc, char *argv[]){
    csh_loop(); //run a infinite loop until the shell is closed
   return EXIT_SUCCESS;
@@ -18,10 +19,37 @@ char *read_line(void){
   if(getline(&input,buff,stdin) == -1){ 
     if(feof(stdin)){ // end of file
       exit(EXIT_SUCCESS);
-    }else{   // we incountred a error probaly because getline cant allocate more memory
+    }else{   //we incountred a error
       perror(error:);
       exit(EXIT_FAILURE);
     }
   }
   return input; 
+}
+ char **CSH_Token(char *line){
+  int position = 0;
+  char buffsize = CSH_TOK_BUFSIZE;
+  char *token;
+  char **tokens;
+  tokens = malloc(buffsize * sizeof(char*));
+  if(!tokens){
+    fprintf(stderr,"csh:error in allocation\n");
+    exit(EXIT_FAILURE);
+  }
+  token =  strtok(line,CSH_TOK_DELIM);
+  while(token != NULL){
+    tokens[position] = token;
+     position++;
+  }
+  if (position > buffsize){
+    buffsize += CSH_TOK_BUFSIZE;
+    tokens = relloc(tokens,buffsize * sizeof(char*));
+    if(!tokens){
+      fprintf(stderr,"csh:error in allocation\n");
+       exit(EXIT_FAILURE);
+    }
+   token = strtok(NULL,CSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
 }
